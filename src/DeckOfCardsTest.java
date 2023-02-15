e9905365baa12d6f887c8b0848906d7b2200d71e
AMD <michojekunle1@gmail.com>
2023-02-15 16:40:42 +0100

    Made changes

---
+++ src/DeckOfCardsTest.java
@@ -0,0 +1,18 @@
+public class DeckOfCardsTest
+{
+    public static void main(String[] args)
+    {
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle(); //place the cards in random order
+
+        //print all 52  Cards in the prder in which they are dealt
+        for(int i = 1; i<=52; i++)
+        {
+            //deal and display card
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
+
+            if(i % 4 == 0) //output a newline after every fourth card
+                System.out.println();
+        }
+    }
+} //end class DeckOFCardsTest
